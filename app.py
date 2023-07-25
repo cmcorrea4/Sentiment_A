b4d9938b45a07eaea6fc56f9cb52502fcdd9cf7b
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-07-25 11:18:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with st.expander('Analizar texto'):
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
         x=round(blob.sentiment.polarity,2)
         if x >= 0.5:
-            st.write( 'Es un sentimiento Positivo',icon=":blush:")
+            st.write( 'Es un sentimiento Positivo',icon="⚠️")
         elif x <= -0.5:
             st.write( 'Es un sentimiento Negativo',icon=':pensive:')
         else:
